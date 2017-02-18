6daa171724e59f0ba788eddbd119e2d919f5fd30
Chen Li <yichen.li0830@gmail.com>
2017-02-17 20:31:17 -0800

    Make use of the Apollo client

---
+++ client/components/SongList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
+
+const SongList = (props) => {
+  console.log("props", props);
+  return <div>Song List</div>
+};
+
+const query = gql`
+{
+  songs
+  {
+    title
+  }
+}
+`;
+
+export default graphql(query)(SongList);
