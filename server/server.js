2aa098280efbc8afcd866bdfd9083b5d6ce4addf
Chen Li <yichen.li0830@gmail.com>
2017-02-17 16:07:11 -0800

    Create a mLab user for the purpose of test. It's not safe and the DB will be deleted soon.

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://dbUser:b>CaKF4)UZzRHR7@ds153669.mlab.com:53669/lyrical-graphql';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
